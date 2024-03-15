6f453073a0e66200f8980be205ca3b3c18099cbb
git <lmarqu358@west-mec.org>
2024-03-14 17:07:11 -0700

    added template

---
+++ main.js
@@ -0,0 +1,16 @@
+// import Game from './scenes/Game'
+
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 500,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 0 }
+        }
+    },
+    scene: [Game]
+}
+
+export default new Phaser.Game(config)

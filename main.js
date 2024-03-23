15484d101b03aff928d9cbd37831838659a779cf
Leo Marquez <lmarqu358@west-mec.org>
2024-03-22 22:12:20 -0700

    need to make tileset

--- main.js
+++ main.js
@@ -1,16 +1,46 @@
-// import Game from './scenes/Game'
+class Preloader extends Phaser.Scene {
+    constructor() {
+        super('preloader')
+    }
 
-const config = {
+    preload() {
+        this.load.image('tiles', 'tiles/dungeon-tiles.png');
+        this.load.tilemapTiledJSON('dungeon', './tiles/dungeon-1.json');
+    }
+
+    create() {
+        this.scene.start('game');
+    }
+}
+
+class Game extends Phaser.Scene {
+    constructor() {
+        super('game');
+    }
+
+    preload() {
+    }
+
+    create() {
+        const map = this.make.tileMap({ key: 'dungeon' });
+        const tileset = map.addTilesetImage('dungeon', 'tiles');
+
+        map.createStaticLayer("Ground", tileset);
+    }
+}
+
+const game = new Phaser.Game({
     type: Phaser.AUTO,
-    width: 800,
-    height: 500,
+    width: 400,
+    height: 250,
     physics: {
         default: 'arcade',
         arcade: {
             gravity: { y: 0 }
         }
     },
-    scene: [Game]
-}
-
-export default new Phaser.Game(config)
+    scene: [Preloader, Game],
+    scale: {
+        zoom: 2
+    }
+})
